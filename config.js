4caba94a2b8f453d61e1b42f57e8b00868681431
Tristan <93832702+cheesetan@users.noreply.github.com>
2023-05-08 13:12:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ const CONFIG = {
 	twelveHourFormat: true,
 
 	// Greetings
-	greetingMorning: 'Good morning!',
+	greetingMorning: 'Good morning,',
 	greetingAfternoon: 'Good afternoon,',
 	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	greetingNight: 'Good night,',
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
